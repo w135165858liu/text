15d6a45bb3e8d8fc966c880983cec6922e1f34f6
liu <1351655858@qq.com>
2019-01-29 20:41:42 +0800

    add .

--- 04-project/苏宁易购/js/core.js
+++ 04-project/苏宁易购/js/core.js
@@ -2,10 +2,15 @@ $('.ad1btn').on('click',function(){
 	$('#topad').toggleClass('topad');
 	$('#ad1btn').toggleClass('ad1btn1');
 });
+/*下拉菜单*/
 $(function(){
-	$('.ng-item').hover(function(){
-		$('.ng-sn-site-na').slideUp(100)
+	$('.ng-item-div').hover(function(){
+		var $this = $(this)
+		var $site = $this.find('>.ng-sn-site-na')
+		$site.slideDown(100)
 	},function(){
-		$('.ng-sn-site-na').slideDown(100)
+		var $this = $(this)
+		var $site = $this.find('>.ng-sn-site-na')		
+		$site.slideUp(100)
 	})
-})
+});
