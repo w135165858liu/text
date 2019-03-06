628d18728980782b17d04b606c4abcace48ba186
liu <1351655858@qq.com>
2019-03-06 18:30:37 +0800

    add .

--- 04-project/ktao/js/search.js
+++ 04-project/ktao/js/search.js
@@ -5,7 +5,8 @@ function Search($elem,options){
 	this.options = options;
 	this.$searchBtn = $elem.find('.search-button');
 	this.$searchInput = $elem.find('.search-text');
-	this.$searchForm = $elem.find('search-form');
+	this.$searchForm = $elem.find('.search-form');
+	this.$searchLayer = $elem.find('.search-layer');
 	//2.初始化
 	this.init();
 	if(this.options.autocompelete){
@@ -28,23 +29,60 @@ Search.prototype = {
 		return $.trim(this.$searchInput.val());
 	},
 	autocompelete:function(){
-		//1.监听输入框input事件
+		//1.监听初始化显示隐藏插件
+		this.$searchLayer.showHide(this.options);
+		//2.监听输入框input事件
 		this.$searchInput.on('input',$.proxy(this.getData,this));
+		//3.点击页面其他地方隐藏下拉层
+		$(document).on('click',$.proxy(this.hideLayer,this));
+		//4.inputh获取焦点时显示下拉层
+		this.$searchInput.on('focus',$.proxy(this.sjhowLayer,this));
+		//5.阻止input上click事件冒泡到document上触发
+		this.$searchLayer.on('click',function(ev){
+			ev.stopPropagation();
+		});
 	},
 	getData:function(){
 		console.log('will get data..');
+		var inputVal = this.getInputVal();
+		if(inputVal == ''){
+			return
+		}
 		$.ajax({
-			url:this.options.url+getInputVal(),
+			url:this.options.url+inputVal,
 			dataType:"jsonp",
 			jsonp:"callback"
+		}).done(function(data){
+			//1.根据数据生成html
+			console.log(data)
+			console.log(this.$searchLayer)
+			var html = '';
+			for(var i=0;i<data.result.length;i++){
+				html += '<li class="search-item">'+data.result[i][0]+'</li>'
+			}
+			//2.加载html到下拉层
+			this.appendHtml(html)
+			//3.显示下拉层
+			this.showLayer()
+		}.bind(this)).fail(function(err){
+			console.log(err)
 		})
-	}
-	https://suggest.taobao.com/sug?code=utf-8&q=xx&_ksTS=1551777113147_2321&callback=jsonp2322&k=1&area=c2c&bucketid=16
+		},
+		showLayer:function(){
+			this.$searchLayer.showHide('show');
+		},
+		appendHtml:function(html){
+			this.$searchLayer.html(html);
+		},
+		hideLayer:function(){
+			this.$searchLayer.showHide('hide');
+		}
 }
 Search.DEFAULTS = {
 	autocompelete:true,
 	url:"https://suggest.taobao.com/sug?&q=",
-
+	js:true,
+	mode:"slideUpDown"
 }
 $.fn.extend({
 	search:function(options){
