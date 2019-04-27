e244488a7c6d09d068c0c235bbf46ab7ac075d5a
liu <1351655858@qq.com>
2019-04-27 19:53:32 +0800

    add .

---
+++ 04-project/kmall/home/src/pages/common/index.js
@@ -0,0 +1,3 @@
+require('node_modules/font-awesome/css/font-awesome.min.css')
+require('./index.css')
+console.log('common ...')
--- 04-project/kmall/home/webpack.config.js
+++ 04-project/kmall/home/webpack.config.js
@@ -2,7 +2,7 @@
 * @Author: TomChen
 * @Date:   2019-04-08 18:41:12
 * @Last Modified by:   TomChen
-* @Last Modified time: 2019-04-24 19:14:08
+* @Last Modified time: 2019-04-15 18:22:20
 */
 const path = require('path');
 const htmlWebpackPlugin = require('html-webpack-plugin')
@@ -10,14 +10,13 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 //css单独打包成一个文件
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const publicPath = "/";
-
 const getHtmlConfig = (name)=>({
-    template:'./src/view/'+name+'.html',//模板文件
-    filename:name+'.html',//输出的文件名
-    inject:true,//脚本写在那个标签里,默认是true(在body结束后)
-    hash:true,//给生成的js/css文件添加一个唯一的hash
-    chunks:['common',name]
-})
+	        template:'./src/view/'+name+'.html',//模板文件
+	        filename:name+'.html',//输出的文件名
+	        inject:true,//脚本写在那个标签里,默认是true(在body结束后)
+	        hash:true,//给生成的js/css文件添加一个唯一的hash
+	        chunks:['common',name]
+	    })
 
 
 module.exports = {
@@ -28,9 +27,9 @@ module.exports = {
 	//单入口写法一
 	entry:{
 		//chunk名称:文件路径
-		'common':'./src/pages/common/index.js',		
 		'index':'./src/pages/index/index.js',		
 		'user-login':'./src/pages/user-login/index.js',		
+		'common':'./src/pages/common/index.js',		
 	},
 	//单入口写法二
 	//entry: './src/index.js',
@@ -44,15 +43,15 @@ module.exports = {
 		path: path.resolve(__dirname, 'dist')
 	},
 	//配置别名
-    resolve:{
-        alias:{
-            pages:path.resolve(__dirname,'./src/pages'),
-            util:path.resolve(__dirname,'./src/util'),
-            service:path.resolve(__dirname,'./src/service'),
-            common:path.resolve(__dirname,'./src/common'),
-            node_modules:path.resolve(__dirname,'./node_modules'),
-        }
-    },	
+	resolve:{
+		alias:{
+			pages:path.resolve(__dirname,'./src/pages'),
+			util:path.resolve(__dirname,'./src/util'),
+			api:path.resolve(__dirname,'./src/api'),
+			common:path.resolve(__dirname,'./src/common'),
+			node_modules:path.resolve(__dirname,'./node_modules'),
+		}
+	},
 	module: {
 		rules: [
 		//处理css文件
@@ -95,7 +94,7 @@ module.exports = {
 	},
 	plugins:[
 	    new htmlWebpackPlugin(getHtmlConfig('index')),
-	    new htmlWebpackPlugin(getHtmlConfig('user-login')),	    
+	    new htmlWebpackPlugin(getHtmlConfig('user-login')),
 	    new CleanWebpackPlugin(),
 	    new MiniCssExtractPlugin({
 	    	filename:'css/[name].css'
@@ -104,9 +103,5 @@ module.exports = {
 	devServer:{
 		contentBase: './dist',//内容的目录
 		port:3002,//服务运行的端口
-		proxy: [{
-		  context: ['/user'],
-		  target: 'http://127.0.0.1:3000',
-		}]		
 	}			
 };
